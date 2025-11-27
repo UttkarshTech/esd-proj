661979412ad05b47b446ab21409479d35f655f43
Uttkarsh Ranjan <utr12.sde@gmail.com>
2025-11-27 18:31:15 +0530

    Init Commit

---
+++ backend/esd-proj/src/test/java/com/uttkarsh/esd_proj/EsdProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uttkarsh.esd_proj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsdProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
